1f7dfd203b36d8eb7a3bcd30568f038ee053e524
Bryden Trakalo <71333735+BrydenTrakalo@users.noreply.github.com>
2022-02-17 17:25:17 -0600

    cleaned deleted code

--- OverWolf-App/ts/src/in_game/in_game.ts
+++ OverWolf-App/ts/src/in_game/in_game.ts
@@ -68,22 +68,7 @@ class InGame extends AppWindow {
   //Sends Matchclock to the html overlay
   //Performs updateData() on Kill and Death events
   private onNewEvents(e) {
-    // const shouldHighlight = e.events.some(event => {
-    //   switch (event.name) {
-    //      case 'kill':
-    //      case 'death':
-    //     // case 'assist':
-    //     // case 'level':
-    //     case 'matchStart':
-    //     case 'match_start':
-    //     case 'matchEnd':
-    //     case 'match_end':
-    //     return true;
-    //   }
-    //   return false;
-    // });
-    //this.logLine(this._eventsLog, e, shouldHighlight);
-
+    //Events -> kill  death assist  level matchStart  match_start matchEnd  match_end
     if(e.events[0]["name"] == 'match_clock'){
       let time_message:string = e.events[0]["data"];
       this.updateData("time", parseInt(time_message, 10));
@@ -97,12 +82,9 @@ class InGame extends AppWindow {
     }
   }
 
-
   //Collects the information written in game_data.txt, builds a javascript object from the data, increments the dataField specified, stringifies the object and writes it back
   private async updateData(dataField:string, time:number){
     let fileData = await this.readFileData(`${overwolf.io.paths.documents}\\GitHub\\Capstone-repo\\Overwolf-App\\ts\\src\\game_data.txt`);
-    //document.getElementById("kill_message").innerHTML = fileData;  //for debugging
-    
     let jsonData = JSON.parse(fileData);
     if(dataField == "kills"){
       jsonData["kills"]++;
@@ -152,50 +134,6 @@ class InGame extends AppWindow {
   private onInfoUpdates(info) {
     //do nothing
   }
-
-  // // Displays the toggle minimize/restore hotkey in the window header
-  // private async setToggleHotkeyText() {
-  //   const gameClassId = await this.getCurrentGameClassId();
-  //   const hotkeyText = await OWHotkeys.getHotkeyText(kHotkeys.toggle, gameClassId);
-  //   const hotkeyElem = document.getElementById('hotkey');
-  //   hotkeyElem.textContent = hotkeyText;
-  // }
-
-  // // Sets toggleInGameWindow as the behavior for the Ctrl+F hotkey
-  // private async setToggleHotkeyBehavior() {
-  //   const toggleInGameWindow = async (
-  //     hotkeyResult: overwolf.settings.hotkeys.OnPressedEvent
-  //   ): Promise<void> => {
-  //     //console.log(`pressed hotkey for ${hotkeyResult.name}`);
-  //     const inGameState = await this.getWindowState();
-
-  //     if (inGameState.window_state === WindowState.NORMAL ||
-  //       inGameState.window_state === WindowState.MAXIMIZED) {
-  //       this.currWindow.minimize();
-  //     } else if (inGameState.window_state === WindowState.MINIMIZED ||
-  //       inGameState.window_state === WindowState.CLOSED) {
-  //       this.currWindow.restore();
-  //     }
-  //   }
-  //   OWHotkeys.onHotkeyDown(kHotkeys.toggle, toggleInGameWindow);
-  // }
-
-  // // Appends a new line to the specified log
-  // private logLine(log: HTMLElement, data, highlight) {
-  //   const line = document.createElement('pre');
-  //   line.textContent = JSON.stringify(data);
-  //   if (highlight) {
-  //     line.className = 'highlight';
-  //   }
-  //   // Check if scroll is near bottom
-  //   const shouldAutoScroll =
-  //     log.scrollTop + log.offsetHeight >= log.scrollHeight - 10;
-
-  //   log.appendChild(line);
-  //   if (shouldAutoScroll) {
-  //     log.scrollTop = log.scrollHeight;
-  //   }
-  // }
 
   private async getCurrentGameClassId(): Promise<number | null> {
     const info = await OWGames.getRunningGameInfo();
