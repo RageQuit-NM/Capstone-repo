328e67f23c848c49eb70ce48c54e6204cd995676
ShaneToma <shanea.toma@gmail.com>
2022-02-26 14:44:42 -0600

    testing collection of stats

--- Server/stats-collection.js
+++ Server/stats-collection.js
@@ -6,6 +6,6 @@ MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var database = db.db("growing_gamers");
     var options = "";
-    var result = database.collection("player_records").find({cellNum: "69"}, options);
+    var result = database.collection("player_records").find({cellNum: 69}, options);
     console.log(JSON.stringify(result));
   });
