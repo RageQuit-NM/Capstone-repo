4a5fe1feb84e36372e7bcb16b1e64b9aabc44dad
ShaneToma <shanea.toma@gmail.com>
2022-02-26 14:25:20 -0600

    not much

---
+++ Server/stats-collection.js
@@ -0,0 +1,11 @@
+
+    var query = {cellNum: "69"};
+    console.log("query is: " + JSON.stringify(query));
+    var result;
+  
+    try {
+      result = await findMany(query).toArray();
+    } catch (error){
+      console.log(error);
+    }
+    console.log("Returning player stats "+ JSON.stringify(result));
