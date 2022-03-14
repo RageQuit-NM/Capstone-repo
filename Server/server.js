86d661a0764cc0b9b79f436bf1f011b447b81bcc
ShaneToma <shanea.toma@gmail.com>
2022-03-14 12:41:43 -0600

    migrating message logic to server

--- Server/server.js
+++ Server/server.js
@@ -197,7 +197,7 @@ app.post('/get-message', async function(req, res){
   console.log("latest Game date is: " + latestGameDate);
 
   //find all games played on the most recent date
-  var regex = "/\A" + latestGameDate + "/i";
+  var regex = new RegExp("/\A" + latestGameDate, "i");
   console.log("regex is: " +  regex);
   query = { cellNum: req.body["cellNum"], timeStamp: regex }
   console.log("query is: " +  JSON.stringify(query));
