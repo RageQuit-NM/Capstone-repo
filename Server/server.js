a9bef02ab99ddf5fb3ebbce5a57260019cf4ffce
ShaneToma <shanea.toma@gmail.com>
2022-03-14 18:20:07 -0600

    generate digests

--- Server/server.js
+++ Server/server.js
@@ -385,7 +385,7 @@ async function dailyDigest(){
   
 
   //2. Generate a daily digest for each subscriber
-  var date = new Date().toISOString().slice(0,10).getUTCDate();
+  var date = new Date().getUTCDate().toISOString().slice(0,10);
   console.log("date is: " + date);
   var cellNum;
   var games;
