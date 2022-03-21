6e3c4888ff3502c6b1d75e82ca848c18fe91b2c8
Bryden Trakalo <71333735+BrydenTrakalo@users.noreply.github.com>
2022-03-20 18:46:36 -0600

    messages fix

--- OverWolf-App/ts/src/launcher/launcher.ts
+++ OverWolf-App/ts/src/launcher/launcher.ts
@@ -23,7 +23,7 @@ class Launcher extends AppWindow {
           document.getElementById("initalization").style.display = "none";
 
           Launcher.instance().setContent();
-          setInterval(Launcher.instance().setContent, 1000*60);
+          setInterval(Launcher.instance().setContent, 1000*2);
 
           //Launcher.instance().collectPreferences();
           //setInterval(Launcher.instance().checkBedtime, 1000*2);
@@ -128,7 +128,7 @@ class Launcher extends AppWindow {
             document.getElementById("initalization").style.display = "none";
 
             Launcher.instance().setContent();
-            setInterval(Launcher.instance().setContent, 1000*60);
+            setInterval(Launcher.instance().setContent, 1000*2);
             Launcher.instance().displayCellNum();
 
             console.log("VAILD_CODE");
